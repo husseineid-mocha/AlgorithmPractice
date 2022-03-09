93157c32e530714138c600ca44403e2192f2f5b9
husseineid-mocha <husseingeid@gmail.com>
2022-03-08 20:54:31 -0800

    stacks and queue

---
+++ stackAndQueues.js
@@ -0,0 +1,91 @@
+// stack is a collection of data
+// LIFO
+// LAST IN FIRST OUT
+// a collection of books
+// a bunch of plates
+
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+    push(val){
+        var newNode = new Node(val);
+        if(!this.first){
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            var temp = this.first;
+            this.first = newNode;
+            this.first.next = temp;
+        }
+        return ++this.size;
+    }
+
+    pop(){
+        if(!this.first) return null;
+        var temp = this.first;
+        if(this.first === this.last){
+            this.last = null;
+        }
+        this.first = this.first.next;
+        this.size--;
+        return temp.value;
+    }
+}
+
+
+class Node {
+    constructor(value) {
+        this.value = value
+        this.next = null
+    }
+}
+
+class Stack {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    push(val) {
+        let newNode = new Node(val)
+
+        if (!this.first) {
+            this.first = newNode
+            this.last = newNode
+        } else {
+            let temp = this.first
+            this.first = newNode
+            this.first.next = temp
+        }
+        return ++this.size
+    }
+
+    pop() {
+        if (!this.first) {
+            return null
+        }
+        if (this.size === 1) {
+            this.first = null
+            this.last = null
+        }
+        let temp = this.first
+        this.first = temp.next
+        this.size--
+        return temp.value
+    }
+}
+
+/* this is not similar to the push and pop for linked list in that the pop has linear time
+since it removes from the end of the list, has to iterate through it, this pop is dealing with the first item, 
+it's constant time */
