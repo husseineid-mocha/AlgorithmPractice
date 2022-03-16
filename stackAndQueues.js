e9b45d9d1eb07bdb53acd0af2ca959539620aec0
husseineid-mocha <husseingeid@gmail.com>
2022-03-15 17:07:38 -0700

    prepostinorder traversals

--- stackAndQueues.js
+++ stackAndQueues.js
@@ -1,136 +1,162 @@
-// stack is a collection of data
-// LIFO
-// LAST IN FIRST OUT
-// a collection of books
-// a bunch of plates
+// // stack is a collection of data
+// // LIFO
+// // LAST IN FIRST OUT
+// // a collection of books
+// // a bunch of plates
 
-class Node {
-    constructor(value){
-        this.value = value;
-        this.next = null;
+// class Node {
+//     constructor(value){
+//         this.value = value;
+//         this.next = null;
+//     }
+// }
+
+// class Stack {
+//     constructor(){
+//         this.first = null;
+//         this.last = null;
+//         this.size = 0;
+//     }
+//     push(val){
+//         var newNode = new Node(val);
+//         if(!this.first){
+//             this.first = newNode;
+//             this.last = newNode;
+//         } else {
+//             var temp = this.first;
+//             this.first = newNode;
+//             this.first.next = temp;
+//         }
+//         return ++this.size;
+//     }
+
+//     pop(){
+//         if(!this.first) return null;
+//         var temp = this.first;
+//         if(this.first === this.last){
+//             this.last = null;
+//         }
+//         this.first = this.first.next;
+//         this.size--;
+//         return temp.value;
+//     }
+// }
+
+
+// class Node {
+//     constructor(value) {
+//         this.value = value
+//         this.next = null
+//     }
+// }
+
+// class Stack {
+//     constructor() {
+//         this.first = null;
+//         this.last = null;
+//         this.size = 0;
+//     }
+
+//     push(val) {
+//         let newNode = new Node(val)
+
+//         if (!this.first) {
+//             this.first = newNode
+//             this.last = newNode
+//         } else {
+//             let temp = this.first
+//             this.first = newNode
+//             this.first.next = temp
+//         }
+//         return ++this.size
+//     }
+
+//     pop() {
+//         if (!this.first) {
+//             return null
+//         }
+//         if (this.size === 1) {
+//             this.first = null
+//             this.last = null
+//         }
+//         let temp = this.first
+//         this.first = temp.next
+//         this.size--
+//         return temp.value
+//     }
+// }
+
+// /* this is not similar to the push and pop for linked list in that the pop has linear time
+// since it removes from the end of the list, has to iterate through it, this pop is dealing with the first item, 
+// it's constant time */
+
+// //? Insertion and removal are both O(1) time
+
+
+// //! Queue
+// // queue is a FIFO, first in first out
+
+// class Node {
+//     constructor(value) {
+//         this.value = value;
+//         this.next = null
+//     }
+// }
+
+// class Queue {
+//     constructor() {
+//         this.first = null
+//         this.last = null
+//         this.size = 0
+//     }
+
+//     enqueue(value) {
+//         let node = new Node(value)
+//         if (!this.head) {
+//             this.first = node
+//             this.last = node
+//         } else {
+//             this.last.next = newNode
+//             this.last = newNode
+//         }
+//         return ++this.size;
+//     }
+
+//     dequeue() {
+//         if (!this.head) return null
+//         if (this.size === 1) {
+//             this.first = null
+//             this.last = null
+//         }
+//         let temp = this.first
+//         this.first = this.first.next
+//         this.size--
+//         return temp.value
+//     }
+// }
+
+
+
+/* 
+Write a function, pairedParentheses, that takes in a string as an argument. The function should return a boolean indicating whether or not the string has well-formed parentheses.
+
+You may assume the string contains only alphabetic characters, '(', or ')'.
+
+const pairedParentheses = (str) => {
+  let count = 0
+  
+  for (let char of str){
+    if (char === '(') {
+      count++
+    } else if (char === ')'){
+        // this line below is to make sure that if you have a 0 and a closer then it's not a vlid parenthesis pair
+      if (count === 0) {
+        return false
+      }
+      count--
     }
-}
+  }
+  return count === 0
+};
 
-class Stack {
-    constructor(){
-        this.first = null;
-        this.last = null;
-        this.size = 0;
-    }
-    push(val){
-        var newNode = new Node(val);
-        if(!this.first){
-            this.first = newNode;
-            this.last = newNode;
-        } else {
-            var temp = this.first;
-            this.first = newNode;
-            this.first.next = temp;
-        }
-        return ++this.size;
-    }
-
-    pop(){
-        if(!this.first) return null;
-        var temp = this.first;
-        if(this.first === this.last){
-            this.last = null;
-        }
-        this.first = this.first.next;
-        this.size--;
-        return temp.value;
-    }
-}
-
-
-class Node {
-    constructor(value) {
-        this.value = value
-        this.next = null
-    }
-}
-
-class Stack {
-    constructor() {
-        this.first = null;
-        this.last = null;
-        this.size = 0;
-    }
-
-    push(val) {
-        let newNode = new Node(val)
-
-        if (!this.first) {
-            this.first = newNode
-            this.last = newNode
-        } else {
-            let temp = this.first
-            this.first = newNode
-            this.first.next = temp
-        }
-        return ++this.size
-    }
-
-    pop() {
-        if (!this.first) {
-            return null
-        }
-        if (this.size === 1) {
-            this.first = null
-            this.last = null
-        }
-        let temp = this.first
-        this.first = temp.next
-        this.size--
-        return temp.value
-    }
-}
-
-/* this is not similar to the push and pop for linked list in that the pop has linear time
-since it removes from the end of the list, has to iterate through it, this pop is dealing with the first item, 
-it's constant time */
-
-//? Insertion and removal are both O(1) time
-
-
-//! Queue
-// queue is a FIFO, first in first out
-
-class Node {
-    constructor(value) {
-        this.value = value;
-        this.next = null
-    }
-}
-
-class Queue {
-    constructor() {
-        this.first = null
-        this.last = null
-        this.size = 0
-    }
-
-    enqueue(value) {
-        let node = new Node(value)
-        if (!this.head) {
-            this.first = node
-            this.last = node
-        } else {
-            this.last.next = newNode
-            this.last = newNode
-        }
-        return ++this.size;
-    }
-
-    dequeue() {
-        if (!this.head) return null
-        if (this.size === 1) {
-            this.first = null
-            this.last = null
-        }
-        let temp = this.first
-        this.first = this.first.next
-        this.size--
-        return temp.value
-    }
-}
+*/
